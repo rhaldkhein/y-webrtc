2c4e48c629851ecef0f241c4b636a8011448e2bc
Kevin Villanueva <undyingmind@gmail.com>
2020-09-17 15:37:13 +0800

    Replaced broadcastchannel

---
+++ src/lib/broadcastchannel.js
@@ -0,0 +1,82 @@
+/* eslint-env browser */
+
+/**
+ * Helpers for cross-tab communication using broadcastchannel with LocalStorage fallback.
+ *
+ * ```js
+ * // In browser window A:
+ * broadcastchannel.subscribe('my events', data => console.log(data))
+ * broadcastchannel.publish('my events', 'Hello world!') // => A: 'Hello world!' fires synchronously in same tab
+ *
+ * // In browser window B:
+ * broadcastchannel.publish('my events', 'hello from tab B') // => A: 'hello from tab B'
+ * ```
+ *
+ * @module broadcastchannel
+ */
+
+// @todo before next major: use Uint8Array instead as buffer object
+
+import * as map from 'lib0/map'
+import { BroadcastChannel } from 'broadcast-channel'
+
+/**
+ * @typedef {Object} Channel
+ * @property {Set<Function>} Channel.subs
+ * @property {any} Channel.bc
+ */
+
+/**
+ * @type {Map<string, Channel>}
+ */
+const channels = new Map()
+
+const BC = BroadcastChannel
+
+/**
+ * @param {string} room
+ * @return {Channel}
+ */
+const getChannel = room =>
+  map.setIfUndefined(channels, room, () => {
+    const subs = new Set()
+    const bc = new BC(room)
+    /**
+     * @param {{data:ArrayBuffer}} e
+     */
+    bc.onmessage = e => subs.forEach(sub => sub(e.data))
+    return {
+      bc, subs
+    }
+  })
+
+/**
+ * Subscribe to global `publish` events.
+ *
+ * @function
+ * @param {string} room
+ * @param {function(any):any} f
+ */
+export const subscribe = (room, f) => getChannel(room).subs.add(f)
+
+/**
+ * Unsubscribe from `publish` global events.
+ *
+ * @function
+ * @param {string} room
+ * @param {function(any):any} f
+ */
+export const unsubscribe = (room, f) => getChannel(room).subs.delete(f)
+
+/**
+ * Publish data to all subscribers (including subscribers on this tab)
+ *
+ * @function
+ * @param {string} room
+ * @param {any} data
+ */
+export const publish = (room, data) => {
+  const c = getChannel(room)
+  c.bc.postMessage(data)
+  c.subs.forEach(sub => sub(data))
+}
